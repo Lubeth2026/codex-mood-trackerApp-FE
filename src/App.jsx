d4712657014b540e32f0aa19185c808382bd6452
Lubeth <lubethjimenez@yahoo.com>
2026-06-26 20:59:47 -0400

    Add getMessage function in FE to fetch data from Flask & display it in React page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [message, setMessage] = useState("");
+
+  useEffect(()=>{
+    async function getMessage() {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/");
+        const data = await response.json();
+        setMessage(data.message)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getMessage();
+  }, [])
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Mood Tracker App</h1>
+      <p>{message}</p>
     </>
   )
 }
